d6d6e0d282c2b6736a1ec8ed1e28324229d0def4
kamil.hapak <kamil.hapak@britenet.com.pl>
2019-12-10 15:30:29 +0100

    add test cases

--- src/test/java/pl/com/britenet/interview/geolocation/LatitudeTest.java
+++ src/test/java/pl/com/britenet/interview/geolocation/LatitudeTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class LatitudeTest {
@@ -13,14 +14,28 @@ class LatitudeTest {
     return Stream.of(90.1, -90.1, -91.0, 91.0);
   }
 
+  private static Stream<Double> getValidLatitude() {
+    return Stream.of(90.0, -89.9999, -89.9999, 0.0, 90.0);
+  }
+
   @ParameterizedTest
   @MethodSource("getInvalidLatitude")
   void shouldFailDueToInvalidLatitude(Double value) {
 
     // then
     assertThrows(
-            IllegalArgumentException.class,
-            // when
-            () -> Latitude.of(value));
+        IllegalArgumentException.class,
+        // when
+        () -> Latitude.of(value));
+  }
+
+  @ParameterizedTest
+  @MethodSource("getValidLatitude")
+  void shouldCreateLatitude(Double value) {
+
+    // then
+    assertDoesNotThrow(
+        // when
+        () -> Latitude.of(value));
   }
 }
--- src/test/java/pl/com/britenet/interview/geolocation/LongitudeTest.java
+++ src/test/java/pl/com/britenet/interview/geolocation/LongitudeTest.java
@@ -5,12 +5,17 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class LongitudeTest {
 
   private static Stream<Double> getInvalidLongitude() {
-    return Stream.of(180.1, -180.1, -181.0, 181.0);
+    return Stream.of(180.1, -180.0001, -181.0, 180.0001);
+  }
+
+  private static Stream<Double> getValidLongitude() {
+    return Stream.of(180.0, -179.9999, -180.0, 0.0);
   }
 
   @ParameterizedTest
@@ -19,8 +24,18 @@ class LongitudeTest {
 
     // then
     assertThrows(
-            IllegalArgumentException.class,
-            // when
-            () -> Longitude.of(value));
+        IllegalArgumentException.class,
+        // when
+        () -> Longitude.of(value));
+  }
+
+  @ParameterizedTest
+  @MethodSource("getValidLongitude")
+  void shouldCreateLongitude(Double value) {
+
+    // then
+    assertDoesNotThrow(
+        // when
+        () -> Longitude.of(value));
   }
 }
